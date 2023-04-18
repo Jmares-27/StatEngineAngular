ac5dc602d869de1e0164658328ab2232785765d9
Derek Zhang <58023992+corporalrivalle@users.noreply.github.com>
2023-04-17 19:22:08 -0700

    Added new libraries to Angular workspace
    Added
    - Reactive forms for login forms
    - Materials for formatting options

--- StatEngine/src/app/app.module.ts
+++ StatEngine/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { LoginPageComponent } from './login-page/login-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
