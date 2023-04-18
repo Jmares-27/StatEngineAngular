a2502bbad4a0a804e0cdbfc9b8f90e285ecf073f
Derek Zhang <58023992+corporalrivalle@users.noreply.github.com>
2023-04-17 19:17:12 -0700

    Generated 2 New pages
    - Landing Page was generated in generic Angular format
    - Login page was generated in generic Angular format

--- StatEngine/src/app/app.module.ts
+++ StatEngine/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
