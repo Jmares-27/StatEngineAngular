0f544acb952698722cf3be388c9e5c3b07b4955b
Nathan Gardner <38595421+ngardner84@users.noreply.github.com>
2023-04-19 18:41:29 -0700

    Added test post functionality in backend

--- backend/index.js
+++ backend/index.js
@@ -10,4 +10,8 @@ app.get("/about", (req, res) => {
     res.send("This tells you about the website");
 });
 
+app.post("/test", (req, res) => {
+    console.log(req)
+});
+
 app.listen(3000,() => console.log("Server listening at port 3000"));
