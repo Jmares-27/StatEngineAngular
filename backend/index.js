d128d3be225dd3599a82e50cf261bc1d1de99969
Nathan Gardner <38595421+ngardner84@users.noreply.github.com>
2023-04-19 17:33:48 -0700

    Created basic express.JS backend

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.get("/about", (req, res) => {
+    res.send("This tells you about the website");
+});
+
+app.listen(3000,() => console.log("Server listening at port 3000"));
