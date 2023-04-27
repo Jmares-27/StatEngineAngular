381baa2400196ae48c17523d486958ec8cf7f7fc
yakaijo <92822314+yakaijo@users.noreply.github.com>
2023-04-27 13:32:49 -0700

    Set Up Register page completed with http request

--- StatEngine/src/app/app.routes.ts
+++ StatEngine/src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { Routes } from "@angular/router";
 import { LoginPageComponent } from "./login-page/login-page.component";
 import { LandingPageComponent } from "./landing-page/landing-page.component";
 import { HomePageComponent } from "./home-page/home-page.component";
-
+import { RegisterPageComponent } from "./register-page/register-page.component";
 export const appRoutes: Routes = [
     {path:'login', component: LoginPageComponent},
+    {path:'register-page', component: RegisterPageComponent},
     {path:'landing-page', component: LandingPageComponent},
     //{path: '**', component: HomePageComponent},
     {path: 'home-page', component: HomePageComponent}
--- StatEngine/src/app/register-page/register-page.component.ts
+++ StatEngine/src/app/register-page/register-page.component.ts
@@ -1,11 +1,52 @@
 import { Component } from '@angular/core';
 import { AuthenticationService } from '../backend-services/authentication.service';
 
+import { FormsModule, FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import {Router} from '@angular/router';
+
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
   styleUrls: ['./register-page.component.css']
 })
 export class RegisterPageComponent {
+  public signupform: FormGroup;
+  constructor( private router:Router, private http: HttpClient, private formBuilder: FormBuilder) {
+    this.signupform = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+
+    });
+  }
+
+
+  // username = "";
+  // email = "";
+  // password = "";
+  display_checker: boolean = false;
+
+  onSubmit() {
+
+    console.log(this.signupform.value);
+
+
+    this.http.post('http://localhost:3005/register', {username: this.signupform.value.username, password: this.signupform.value.password, email: this.signupform.value.email })
+    .subscribe(
+      data => {
+        console.log(data);
+        // this.router.navigate(['/homepage']);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+
+    this.display_checker = true;
+  }
+
+
 
 }
+
