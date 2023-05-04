288659c4ae22931bbd2f1d8ba2bdb4734dea6558
yakaijo <92822314+yakaijo@users.noreply.github.com>
2023-05-03 19:55:56 -0700

    Wroking on Search player component

--- StatEngine/src/app/search-player/search-player.component.ts
+++ StatEngine/src/app/search-player/search-player.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
-
+import { HttpClient , HttpEvent, HttpRequest} from '@angular/common/http';
+import { FormsModule, FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-search-player',
   templateUrl: './search-player.component.html',
   styleUrls: ['./search-player.component.css']
 })
 export class SearchPlayerComponent {
+  username = new FormControl ('', Validators.required);
+  status_checker = false
+  message = ""
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
+
+      
+  onSubmit() {
+
+    console.log(this.username);
+
+
+    this.http.post('http://localhost:3005/search', this.username)
+    .subscribe(
+      data => {
+        console.log(data);
+        // this.router.navigate(['/homepage']);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+
+  }
+
+
 
 }
+
