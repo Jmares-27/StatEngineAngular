5a858a081afbc81449b6e53fcab548c2a8a4ed3e
Johnnyded <johnny.p828@yahoo.com>
2023-06-02 16:09:10 -0700

    Changed form hints to validator error messages

--- StatEngine/src/app/register/register.component.ts
+++ StatEngine/src/app/register/register.component.ts
@@ -56,4 +56,13 @@ export class RegisterComponent {
     )
 
   }
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
 }
