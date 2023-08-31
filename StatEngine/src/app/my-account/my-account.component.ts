ee11d259c70835f1d64bb5f03c61d61d24d66a2d
JOHNNIE MARES <johnniemares@dhcp-39-3-0.dhcpint.csulb.edu>
2023-08-30 18:11:42 -0700

    added variables for use later

--- StatEngine/src/app/my-account/my-account.component.ts
+++ StatEngine/src/app/my-account/my-account.component.ts
@@ -9,36 +9,10 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./my-account.component.css']
 })
 export class MyAccountComponent {
-  message = ""
-  status_checker = false
-  searchString = ""
-  deleteForm;
-  constructor(private formBuilder: FormBuilder, private http: HttpService, private router: Router){
-    // searchString: String;
-    this.deleteForm = this.formBuilder.group({
-      username:['',[Validators.required]],
-    });
-  }
-
-  onSubmit(){
-    this.http.deleteAccount(this.deleteForm.value.username ).subscribe(
-      data=>{
-        // console.log("HERE -->", data);
-        if ( !data ) {
-          // console.log("inside no data") //used for testing
-          this.status_checker = true
-          this.message = "There is no such player exist"
-        }
-        else{
-          
-          console.log("user data-->", data)
-          this.status_checker = true
-          this.message = "Delete successfully!!"
-          this.http.logOut();
-        }
-      },
-      error => console.log(error)
-    )
-  }
+  
+  userId: number = null;
+  userName: string = null;
+  
+  
  
 }
