1dd93ea1e65f381be484672523a65e1a4c3af2f7
Johnnyded <johnny.p828@yahoo.com>
2023-12-03 13:35:39 -0800

    Revert "Merge branch 'main' of https://github.com/Jmares-27/StatEngineAngular"
    This reverts commit 8a0233688923e83cb9c9b2b89f5472426f997fc2, reversing
    changes made to 6ab7c8c1b25a1db2d558b1e488d082f855e01905.

--- StatEngine/src/app/app.component.ts
+++ StatEngine/src/app/app.component.ts
@@ -9,11 +9,6 @@ import {FormBuilder, Validators, FormControl, FormGroup} from '@angular/forms';
 import { User } from './models/user.model';
 import { MatAutocomplete } from '@angular/material/autocomplete';
 import { AuthGuard } from './_services/authGuard';
-import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
-import { ElementRef} from '@angular/core';
-import { MatDrawerMode } from '@angular/material/sidenav';
-
-
 // import { UserService } from './user/user.component';
 @Component({
   selector: 'app-root',
@@ -27,13 +22,11 @@ export class AppComponent {
   // displayRegAndLogin: boolean  = false;
   hasLoggedIn :boolean = false;
   public SearchForm: FormGroup;
-  drawerMode: MatDrawerMode = 'over'; // 'side' or 'over'
-  viewportState = "" 
 
   searchString: string =  "";
   showSuggestions: boolean = false;
   suggestions: any = [];
-  constructor(private _elementRef : ElementRef, private breakpointObserver:BreakpointObserver, private authGuard:AuthGuard, private searchService:SearchService, private formBuilder:FormBuilder,private http:HttpService, private router: Router, private snackBar: MatSnackBar){
+  constructor(private authGuard:AuthGuard, private searchService:SearchService, private formBuilder:FormBuilder,private http:HttpService, private router: Router, private snackBar: MatSnackBar){
     if (this.checkAuthenication() == true) {
       this.hasLoggedIn = true;
     }else {
@@ -44,24 +37,6 @@ export class AppComponent {
 
     this.SearchForm = this.formBuilder.group({
       username:['',[Validators.required]],
-    });
-
-
-
-      // detect screen size changes
-      // we will only have a collapsable sidebar on mobile and tablet
-    this.breakpointObserver.observe([
-      "(max-width: 1024px)"
-    ]).subscribe((result: BreakpointState) => {
-      if (result.matches) {
-          // hide stuff   
-          this.drawerMode = "over"
-          this.viewportState = "mobileOrTablet"
-        } else {
-          // show stuff
-          this.drawerMode = "side"
-          this.viewportState = "laptopOrDesktop"
-      }
     });
 
   }
@@ -97,29 +72,22 @@ export class AppComponent {
   homeRedirect(){
     this.router.navigate(['home']);
     this.menuToggle();
-    document.getElementById("searchbar1").style.display="block";
-
   }
 
   userPageRedirect(selectedSuggestion){
     this.router.navigate(['user', selectedSuggestion["_id"]])
     this.SearchForm.reset()
     this.suggestions = []
-    document.getElementById("searchbar1").style.display="block";
-
   }
 
   registerRedirect(){
     this.router.navigate(['register']);
     this.menuToggle();
-    document.getElementById("searchbar1").style.display="none";
-
   }
 
   loginRedirect(){
     this.router.navigate(['login']);
     this.menuToggle();
-    document.getElementById("searchbar1").style.display="none";
 
   }
 
@@ -127,22 +95,17 @@ export class AppComponent {
   resetPasswordRedirect(){
     this.router.navigate(['passwordreset']);
     this.menuToggle();
-    document.getElementById("searchbar1").style.display="block";
-
   }
 
   myAccountRedirect(){
     this.router.navigate(['myaccount']);
     this.menuToggle();
-    document.getElementById("searchbar1").style.display="block";
-
 
   }
   
   searchRedirect(){
     this.router.navigate(['search'])
     this.menuToggle();
-
 
   }
 
@@ -166,48 +129,34 @@ export class AppComponent {
     this.snackBar.open("Logging out! Redirecting...","",{duration: 2000});
     this.router.navigate(["home"]);
     this.menuToggle();
-    
   }
   
   deleteAccountRedirect(){
     this.router.navigate(['deleteaccount'])
-    document.getElementById("searchbar1").style.display="block";
-
   }
 
-  //this.opened true/false
   menuToggle():boolean{
-    if(this.viewportState == "mobileOrTablet"){
-      this.opened=!this.opened
-    }
+    this.opened=!this.opened
     return this.opened
   }
 
   bugReportRedirect(){
     this.router.navigate(['bugreport'])
     this.menuToggle();
-    document.getElementById("searchbar1").style.display="block";
-
   }
 
   bugReportSuccessRedirect(){
     this.router.navigate(['bugreportsuccess'])
     this.menuToggle();
-    document.getElementById("searchbar1").style.display="block";
-
   }
 
   favoritesRedirect(){
     this.router.navigate(['favorites'])
     this.menuToggle();
-    document.getElementById("searchbar1").style.display="block";
-
   }
 
   myInventoryRedirect(){
     this.router.navigate(['myinventory'])
     this.menuToggle();
-    document.getElementById("searchbar1").style.display="block";
-
   }
 }
